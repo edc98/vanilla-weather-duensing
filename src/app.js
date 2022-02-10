50763219919f37fbf0eaa3c8233ce030261dc3ac
edc98 <emduensing@gmail.com>
2022-02-10 09:24:23 -0500

    added displayForecast() function in app.js
    used foreach function through each day in created array to create forecast for each new day without copying code 5 times

--- src/app.js
+++ src/app.js
@@ -98,3 +98,30 @@ function convertC(event){
 
 let currentC = document.querySelector("#celcius");
 currentC.addEventListener("click", convertC);
+
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class = "row">`;
+    let days = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
+    days.forEach(function(day) {
+        forecastHTML = forecastHTML + `
+                <div class = "col-2">
+                    <div class = "weather-forecast-date">
+                        ${day}
+                    </div>
+                    <img src = "http://openweathermap.org/img/wn/10d@2x.png" alt = "" width = 36px />
+                    <div class = "weather-forecast-temperature">
+                        <span class = "weather-forecast-temp-max">
+                            18°
+                        </span> 
+                        <span class = "weather-forecast-temp-min">
+                            12°
+                        </span>
+                    </div>
+                </div>`;
+    });
+    forecastHTML = forecastHTML + `</div`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
